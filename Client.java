90cb340ebcc0abac8743125ec8fc4e66565f9956
`rinya <rinyasingh@gmail.com>
2023-09-26 13:01:50 +0200

    basic client/server

--- Client.java
+++ Client.java
@@ -1,9 +1,48 @@
-package networkSecPractical;
+
 import java.net.*;
 import java.io.*;
 
 
 
 public class Client {
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
 
+    public Client (String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+            input = new DataInputStream(System.in);
+
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException u) {
+            System.out.println(u);
+            return;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        String line = "";
+        while (!line.equals("Over")) {
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+            } catch (IOException i ){
+                System.out.println(i);
+            }
+        }
+
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch (IOException i) {
+            System.out.print(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        Client client = new Client("127.0.0.1", 5000);
+    }
 }
