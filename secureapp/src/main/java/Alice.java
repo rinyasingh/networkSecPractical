c6fc45a4f89270af276854ff0457119bac74e057
beyonsay <73654501+beyonsay@users.noreply.github.com>
2023-10-10 16:53:10 +0200

    Working private key encrypt public key decrypt

--- secureapp/src/main/java/Alice.java
+++ secureapp/src/main/java/Alice.java
@@ -1,19 +1,25 @@
 import java.io.*;
 import java.net.*;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Scanner;
 import keys.KeyUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.FileReader;
-import java.security.KeyPair;
+import java.util.jar.JarException;
+
 public class Alice {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        PublicKey alicePub;
-        PrivateKey alicePriv;
-        PublicKey bobPub;
+        PublicKey alicePub = null;
+        PrivateKey alicePriv = null;
+        PublicKey bobPub = null;
         try{
 
              alicePub = KeyUtils.readPublicKey("alice");
@@ -32,15 +38,33 @@ public class Alice {
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
             //RECEIVE MESSAGES FROM BOB
+            PublicKey finalBobPub = bobPub;
             Thread bobListener = new Thread(() -> {
                 try {
                     DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
                     while (true) {
                         String receivedMessage = dataInputStream.readUTF();
-                        System.out.println("Bob: "+ receivedMessage);
+
+                        // Decrypt with the recipient's public key
+                        Cipher decryptCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+                        decryptCipher.init(Cipher.DECRYPT_MODE, finalBobPub);
+                        byte[] decryptedBytes = decryptCipher.doFinal(Base64.decode(receivedMessage));
+                        String decryptedMessage = new String(decryptedBytes, "UTF-8");
+
+                        System.out.println("Bob: "+ decryptedMessage);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
+                } catch (NoSuchPaddingException e) {
+                    throw new RuntimeException(e);
+                } catch (IllegalBlockSizeException e) {
+                    throw new RuntimeException(e);
+                } catch (NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                } catch (BadPaddingException e) {
+                    throw new RuntimeException(e);
+                } catch (InvalidKeyException e) {
+                    throw new RuntimeException(e);
                 }
             });
             bobListener.start();
@@ -49,8 +73,29 @@ public class Alice {
             while (true) {
                 String message = scanner.nextLine();
                 //Checks so it doesnt send empty messages
+
+                //1. Hash message - https://www.geeksforgeeks.org/sha-256-hash-in-java/
+
+                // Static getInstance method is called with hashing SHA
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+                // digest() method called to calculate message digest of an input and return array of byte
+                byte[] digest = md.digest(message.getBytes(StandardCharsets.UTF_8));
+
+                //Convert message to bytes
+                byte[] data = message.getBytes();
+
+                //join message and digest
+
+                //2. ENCRYPT HASHED MESSAGE WITH PRIVATE KEY
+                Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, alicePriv);
+                byte[] privEncryptedMessage = cipher.doFinal(data);//encrypts
+
+
                 if (!message.isEmpty()) {
-                    dataOutputStream.writeUTF(message);
+                    dataOutputStream.writeUTF(Base64.toBase64String(privEncryptedMessage));
+                    System.out.println("sent:"+privEncryptedMessage.toString());
                 }
                 if (message.equalsIgnoreCase("exit")) {
                     break;
@@ -60,6 +105,14 @@ public class Alice {
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- secureapp/src/main/java/Bob.java
+++ secureapp/src/main/java/Bob.java
@@ -1,18 +1,21 @@
 import keys.KeyUtils;
+import org.bouncycastle.util.encoders.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 import java.net.*;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
 import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
-
-
-        PublicKey bobPub;
-        PrivateKey bobPriv;
-        PublicKey alicePub;
+        PublicKey bobPub = null;
+        PrivateKey bobPriv = null;
+        PublicKey alicePub = null;
         try{
 
             bobPub = KeyUtils.readPublicKey("bob");
@@ -30,15 +33,34 @@ public class Bob {
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
             //RECEIVE MESSAGES FROM ALICE
+            PublicKey finalAlicePub = alicePub;
+            System.out.println(finalAlicePub);
             Thread aliceListener = new Thread(() -> {
                 try {
                     DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
                     while (true) {
                         String receivedMessage = dataInputStream.readUTF();
-                        System.out.println("Alice: " + receivedMessage);
+
+                        // Decrypt with the recipient's public key
+                        Cipher decryptCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+                        decryptCipher.init(Cipher.DECRYPT_MODE, finalAlicePub);
+                        byte[] decryptedBytes = decryptCipher.doFinal(Base64.decode(receivedMessage));
+                        String decryptedMessage = new String(decryptedBytes, "UTF-8");
+
+                        System.out.println("Alice: " + decryptedMessage);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
+                } catch (NoSuchPaddingException e) {
+                    throw new RuntimeException(e);
+                } catch (IllegalBlockSizeException e) {
+                    throw new RuntimeException(e);
+                } catch (NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                } catch (BadPaddingException e) {
+                    throw new RuntimeException(e);
+                } catch (InvalidKeyException e) {
+                    throw new RuntimeException(e);
                 }
             });
             aliceListener.start();
@@ -46,11 +68,31 @@ public class Bob {
             //SEND MESSAGES TO ALICE
             while (true) {
                 String message = scanner.nextLine();
-                // Check so it doesnt send empty messages
-                if (!message.isEmpty()) {
-                    dataOutputStream.writeUTF(message);
-                }
+                //Checks so it doesnt send empty messages
 
+                //1. Hash message - https://www.geeksforgeeks.org/sha-256-hash-in-java/
+
+                // Static getInstance method is called with hashing SHA
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+                // digest() method called to calculate message digest of an input and return array of byte
+                byte[] digest = md.digest(message.getBytes(StandardCharsets.UTF_8));
+
+                //Convert message to bytes
+                byte[] data = message.getBytes();
+
+                //join message and digest
+
+                //2. ENCRYPT HASHED MESSAGE WITH PRIVATE KEY
+                Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, bobPriv);
+                byte[] privEncryptedMessage = cipher.doFinal(data);//encrypts
+
+
+                if (!message.isEmpty()) {
+                    dataOutputStream.writeUTF(Base64.toBase64String(privEncryptedMessage));
+                    System.out.println("sent:"+privEncryptedMessage.toString());
+                }
                 if (message.equalsIgnoreCase("exit")) {
                     break;
                 }
@@ -59,6 +101,16 @@ public class Bob {
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
         }
     }
 }
